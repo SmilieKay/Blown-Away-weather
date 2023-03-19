a393fcd8f6c4738eda11a9c3c7be2bb767d68644
SmilieKay <smilie_kay@yahoo.com>
2023-03-19 04:24:20 -0500

    fixed the current day and cleaned up code a bit

--- assets/index.js
+++ assets/index.js
@@ -1,61 +1,55 @@
-let searchInp = document.querySelector(".weather-search");
-let city = document.querySelector(".weather-city");
-let day = document.querySelector(".weather-day");
-let humidity = document.querySelector(".weather-indicator-humidity>.value");
-let wind = document.querySelector(".weather-indicator-wind>.value");
 
-let tempHigh = document.querySelector(".weather-indicator-temp-high>.value");
-let tempLow = document.querySelector(".weather-indicator-temp-low>.value");
 
-let image = document.querySelector(".weather-image");
-let temp = document.querySelector(".weather-temp>.value");
-let weatherAPIKey = "2e4914512e314cd0b4ee221b3961702a";
-let weatherBaseEndpoint =
-  "https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=" +
-  weatherAPIKey;
-let weatherForecastEndpoint =
-  "https://api.openweathermap.org/data/2.5/forecast?units=imperial&appid=" +
-  weatherAPIKey;
-let forecastSection = document.querySelector(".weather-forecast");
-let searchHistory = document.querySelector(".search-history");
+const searchInp = document.querySelector(".weather-search");
+const city = document.querySelector(".weather-city");
+const currentDay = document.querySelector(".weather-day");
+const humidity = document.querySelector(".weather-indicator-humidity>.value");
+const wind = document.querySelector(".weather-indicator-wind>.value");
+const tempHigh = document.querySelector(".weather-indicator-temp-high>.value");
+const tempLow = document.querySelector(".weather-indicator-temp-low>.value");
+const image = document.querySelector(".weather-image");
+const temp = document.querySelector(".weather-temp>.value");
+const weatherAPIKey = "2e4914512e314cd0b4ee221b3961702a";
+const weatherBaseEndpoint = `https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=${weatherAPIKey}`;
+const weatherForecastEndpoint = `https://api.openweathermap.org/data/2.5/forecast?units=imperial&appid=${weatherAPIKey}`;
+const forecastSection = document.querySelector(".weather-forecast");
+const searchHistory = document.querySelector(".search-history");
 
-let getWeatherByCityName = async (city) => {
-    let endpoint = `${weatherBaseEndpoint}&q=${city}`;
-    let response = await fetch(endpoint);
-    
-    if (response.ok) {
-      let weather = await response.json();
-      return weather;
-    } else {
-      showError("City not found. Please try another city.");
-      return null;
-    }
-  };
+const getWeatherByCityName = async (city) => {
+  const endpoint = `${weatherBaseEndpoint}&q=${city}`;
+  const response = await fetch(endpoint);
 
-  const showError = (message) => {
-    const errorMessageContainer = document.querySelector(".error-message");
-    errorMessageContainer.textContent = message;
-    errorMessageContainer.style.display = "block";
-  
-    setTimeout(() => {
-      errorMessageContainer.style.display = "none";
-    }, 3000);
-  };
-  
-  
-  let getForecastByCityName = async (city) => {
-    let endpoint = `${weatherForecastEndpoint}&q=${city}`;
-    let response = await fetch(endpoint);
-  
-    if (response.ok) {
-      let forecast = await response.json();
-      return forecast;
-    } else {
-      showError("City not found. Please try another city.");
-      return null;
-    }
-  };
-  
+  if (response.ok) {
+    const weather = await response.json();
+    return weather;
+  } else {
+    showError("City not found. Please try another city.");
+    return null;
+  }
+};
+
+const showError = (message) => {
+  const errorMessageContainer = document.querySelector(".error-message");
+  errorMessageContainer.textContent = message;
+  errorMessageContainer.style.display = "block";
+
+  setTimeout(() => {
+    errorMessageContainer.style.display = "none";
+  }, 3000);
+};
+
+const getForecastByCityName = async (city) => {
+  const endpoint = `${weatherForecastEndpoint}&q=${city}`;
+  const response = await fetch(endpoint);
+
+  if (response.ok) {
+    const forecast = await response.json();
+    return forecast;
+  } else {
+    showError("City not found. Please try another city.");
+    return null;
+  }
+};
 
 const storeSearch = (city) => {
   let searches = JSON.parse(localStorage.getItem("searches")) || [];
@@ -78,9 +72,7 @@ const displaySearchHistory = () => {
     searchHistory.appendChild(searchItem);
   });
 
-  // Add the event listener to the parent element
   searchHistory.addEventListener("click", async (event) => {
-    // Check if the clicked element has the class 'search-item'
     if (event.target.classList.contains("search-item")) {
       let cityName = event.target.textContent;
       let weather = await getWeatherByCityName(cityName);
@@ -92,71 +84,74 @@ const displaySearchHistory = () => {
   });
 };
 
-
-searchInp.addEventListener("keydown", async (event) => {
+const addSearchEventListener = () => {
+  searchInp.addEventListener("keydown", async (event) => {
     if (event.key === "Enter") {
       let cityName = searchInp.value;
-  
+
       let weather = await getWeatherByCityName(cityName);
-  
+
       if (weather) {
         storeSearch(cityName);
         displaySearchHistory();
         updateCurrentWeather(weather);
-  
+
         let forecast = await getForecastByCityName(cityName);
         updateForecast(forecast);
       }
+      // Clear the search input after processing the search
+      searchInp.value = "";
     }
   });
-  
-
-const getWeatherIcon = (iconCode) => {
-  switch (iconCode) {
-    case "01d":
-      return "./images/sunny.png";
-    case "01n":
-      return "images/half-moon.png";
-    case "02d":
-      return "images/cloudy.png";
-    case "02n":
-      return "images/partly-cloudy.png";
-    case "03d":
-      return "images/partly-cloudy.png";
-    case "03n":
-      return "images/night-cloud.png";
-    case "04d":
-      return "images/partly-cloudy.png";
-    case "04n":
-      return "images/night-cloud.png";
-    case "09d":
-      return "images/rain.png";
-    case "09n":
-      return "images/rainy-night.png";
-    case "10d":
-      return "images/rain.png";
-    case "10n":
-      return "images/rainy-night.png";
-    case "11d":
-      return "images/storm.png";
-    case "11n":
-      return "images/storm.png";
-    case "13d":
-      return "images/snow.png";
-    case "13n":
-      return "images/snow.png";
-    case "50d":
-      return "images/foggy.png";
-    case "50n":
-      return "images/foggy.png";
-    default:
-      return "./images/sunny.png";
-  }
 };
 
-let updateCurrentWeather = (data, forecastData) => {
+const getWeatherIcon = (iconCode) => {
+    switch (iconCode) {
+      case "01d":
+        return "./images/sunny.png";
+      case "01n":
+        return "images/half-moon.png";
+      case "02d":
+        return "images/cloudy.png";
+      case "02n":
+        return "images/partly-cloudy.png";
+      case "03d":
+        return "images/partly-cloudy.png";
+      case "03n":
+        return "images/night-cloud.png";
+      case "04d":
+        return "images/partly-cloudy.png";
+      case "04n":
+        return "images/night-cloud.png";
+      case "09d":
+        return "images/rain.png";
+      case "09n":
+        return "images/rainy-night.png";
+      case "10d":
+        return "images/rain.png";
+      case "10n":
+        return "images/rainy-night.png";
+      case "11d":
+        return "images/storm.png";
+      case "11n":
+        return "images/storm.png";
+      case "13d":
+        return "images/snow.png";
+      case "13n":
+        return "images/snow.png";
+      case "50d":
+        return "images/foggy.png";
+      case "50n":
+        return "images/foggy.png";
+      default:
+        return "./images/sunny.png";
+    }
+  };
+  
+
+
+const updateCurrentWeather = (data, forecastData) => {
   city.textContent = `${data.name}, ${data.sys.country}`;
-  day.textContent = dayOfWeek();
   humidity.textContent = `Humidity: ${data.main.humidity}`;
   tempHigh.textContent = Math.round(data.main.temp_max);
   tempLow.textContent = Math.round(data.main.temp_min);
@@ -181,53 +176,58 @@ let updateCurrentWeather = (data, forecastData) => {
   let weatherIcon = getWeatherIcon(data.weather[0].icon);
   image.src = weatherIcon;
   image.alt = data.weather[0].description;
+  //sets current day
+  currentDay.textContent = new Date().toLocaleDateString('en-US',{ weekday: 'long'});
 };
 
-let updateForecast = (data) => {
-    forecastSection.innerHTML = "";
-    for (let i = 0; i < data.list.length; i += 8) {
-      let forecast = data.list[i];
-      let forecastElement = document.createElement("div");
-      forecastElement.classList.add("weather-forecast-item");
-  
-      let forecastDate = new Date(forecast.dt * 1000).toLocaleDateString(
-        "en-EN",
-        {
-          month: "short",
-          day: "numeric",
-        }
-      );
-      let forecastTemp = Math.round(forecast.main.temp);
-      let forecastIcon = getWeatherIcon(forecast.weather[0].icon);
-      let forecastHumidity = forecast.main.humidity; // Add humidity
-      let forecastWindSpeed = forecast.wind.speed; // Add wind speed
-  
-      forecastElement.innerHTML = `
-          <div class="weather-forecast-date">${forecastDate}</div>
-          <img src="${forecastIcon}" alt="${forecast.weather[0].description}" class="weather-forecast-icon">
-          <div class="weather-forecast-temp">${forecastTemp}&#176;</div>
-          <div class="weather-forecast-humidity">Humidity: ${forecastHumidity}%</div> <!-- Add humidity element -->
-          <div class="weather-forecast-wind">Wind: ${forecastWindSpeed} mph</div> <!-- Add wind speed element -->
-      `;
-      forecastSection.appendChild(forecastElement);
-    }
-  };
-  
+const updateForecast = (data) => {
+  forecastSection.innerHTML = "";
+  for (let i = 0; i < data.list.length; i += 8) {
+    let forecast = data.list[i];
+    let forecastElement = document.createElement("div");
+    forecastElement.classList.add("weather-forecast-item");
 
-let dayOfWeek = () => {
-  return new Date().toLocaleDateString("en-EN", { weekday: "long" });
+    let forecastDate = new Date(forecast.dt * 1000).toLocaleDateString(
+      "en-EN",
+      {
+        month: "short",
+        day: "numeric",
+      }
+    );
+    let forecastTemp = Math.round(forecast.main.temp);
+    let forecastIcon = getWeatherIcon(forecast.weather[0].icon);
+    let forecastHumidity = forecast.main.humidity;
+    let forecastWindSpeed = forecast.wind.speed;
+
+    forecastElement.innerHTML = `
+        <div class="weather-forecast-date">${forecastDate}</div>
+        <img src="${forecastIcon}" alt="${forecast.weather[0].description}" class="weather-forecast-icon">
+        <div class="weather-forecast-temp">${forecastTemp}&#176;</div>
+        <div class="weather-forecast-h
+        -humidity">Humidity: ${forecastHumidity}%</div>
+        <div class="weather-forecast-wind">Wind: ${forecastWindSpeed} mph</div>
+    `;
+
+    forecastSection.appendChild(forecastElement);
+  }
 };
+
+
 
 const loadInitialData = async () => {
-  const defaultCity = "Litchfield";
+    const defaultCity = "Litchfield";
+  
+    let weather = await getWeatherByCityName(defaultCity);
+    updateCurrentWeather(weather);
+  
+    let forecast = await getForecastByCityName(defaultCity);
+    updateForecast(forecast);
+  
+    displaySearchHistory();
+  };
+  
+  loadInitialData();
+  
 
-  let weather = await getWeatherByCityName(defaultCity);
-  updateCurrentWeather(weather);
-
-  let forecast = await getForecastByCityName(defaultCity);
-  updateForecast(forecast);
-
-  displaySearchHistory();
-};
-
-loadInitialData();
+displaySearchHistory();
+addSearchEventListener();
